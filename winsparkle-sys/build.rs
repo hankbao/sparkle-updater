40eafd20ff6ee7beb4b16ef5b1077092c792b7e9
Hank Bao <hankbao@W11Dev.HyperV>
2023-03-13 02:35:29 -0700

    fix winsparkle-sys linkage

--- winsparkle-sys/build.rs
+++ winsparkle-sys/build.rs
@@ -3,5 +3,5 @@
 
 fn main() {
     let dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
-    println!("cargo:rustc-link-search=native={}", dir);
+    println!("cargo:rustc-link-search=dylib={}", dir);
 }
