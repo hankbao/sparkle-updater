58929e07dc93acf1bc9d619f9839626a2bbda27c
Hank Bao <hankbao@W11Dev.HyperV>
2023-03-13 22:13:56 -0700

    update Updater implementation on windows

--- updater/src/lib.rs
+++ updater/src/lib.rs
@@ -6,8 +6,9 @@ use std::ffi::c_void;
 
 #[cfg(target_os = "windows")]
 use winsparkle_sys::{
-    win_sparkle_check_update_with_ui, win_sparkle_init, win_sparkle_set_appcast_url,
-    win_sparkle_set_registry_path, win_sparkle_set_shutdown_request_callback,
+    win_sparkle_check_update_with_ui, win_sparkle_cleanup, win_sparkle_init,
+    win_sparkle_set_appcast_url, win_sparkle_set_dsa_pub_pem, win_sparkle_set_registry_path,
+    win_sparkle_set_shutdown_request_callback,
 };
 
 #[cfg(target_os = "macos")]
@@ -63,6 +64,43 @@ impl Updater {
         Self {}
     }
 
+    /// Create a new `Updater` instance on Windows.
+    ///
+    /// Creates a new `Updater` instance for Windows using the WinSparkle dynamic-link library (DLL).
+    /// Takes the URL for the appcast file, the path in the registry where WinSparkle will store its settings,
+    /// and an optional callback function for handling shutdown requests.
+    ///
+    /// * `appcast_url` - A string slice containing the URL for the appcast file. This specifies the URL where WinSparkle will look for updates.
+    ///
+    /// * `registry_path` - A string slice containing the path in the registry where WinSparkle will store its settings. This sets the path where WinSparkle will store its settings in the registry.
+    ///
+    /// * `dsa_pub_pem` - A string slice containing the DSA public key. Only PEM format is supported. Public key will be used to verify DSA signatures of the update file.
+    ///
+    /// * `shutdown_request_callback` - An optional callback function that WinSparkle will call when it receives a request to shut down the application during an update. The callback should return `c_void`.
+    #[cfg(target_os = "windows")]
+    pub fn new_with_pubkey(
+        appcast_url: &str,
+        registry_path: &str,
+        dsa_pub_pem: &str,
+        shutdown_request_callback: Option<extern "C" fn() -> c_void>,
+    ) -> Self {
+        use std::ffi::CString;
+
+        let appcast_url_cstr = CString::new(appcast_url).unwrap();
+        let registry_path_cstr = CString::new(registry_path).unwrap();
+        let dsa_pub_pem_cstr = CString::new(dsa_pub_pem).unwrap();
+
+        unsafe {
+            win_sparkle_set_appcast_url(appcast_url_cstr.as_ptr());
+            win_sparkle_set_registry_path(registry_path_cstr.as_ptr());
+            win_sparkle_set_dsa_pub_pem(dsa_pub_pem_cstr.as_ptr());
+            win_sparkle_set_shutdown_request_callback(shutdown_request_callback);
+            win_sparkle_init();
+        }
+
+        Self {}
+    }
+
     /// Check for app updates
     #[cfg(target_os = "macos")]
     pub fn check_for_updates(&self) {
@@ -74,6 +112,15 @@ impl Updater {
     pub fn check_for_updates(&self) {
         unsafe {
             win_sparkle_check_update_with_ui();
+        }
+    }
+}
+
+#[cfg(target_os = "windows")]
+impl Drop for Updater {
+    fn drop(&mut self) {
+        unsafe {
+            win_sparkle_cleanup();
         }
     }
 }
